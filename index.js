48c7584ab2b228dffe88e659551b06cea9e736bf
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2023-01-25 10:31:32 -0700

    created Employee.js, Engineer.js, Intern.js and Manager.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const employee = require("./lib/Employee");
+const engineer = require("./lib/Engineer");
+const intern = require("./lib/Intern");
+const manager = require("./lib/Manager");
