5c40c32d0848d0b4a11761ee2a40f454eeb25477
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2023-01-26 17:31:56 -0700

    completed the html outline for the employee cards

--- src/employeeCardInfo.js
+++ src/employeeCardInfo.js
@@ -1,4 +1,6 @@
+// Adds the cards to the html outline below.
 const makeCard = (empCard) => {
+    // HTML outline for Manager card information.
     const makeManager = (manager) => {
         return `
         <div class="card col d-flex justify-content-center p-3 m-5" style="width: 18rem;">
@@ -9,20 +11,77 @@ const makeCard = (empCard) => {
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">${manager.getId()}</li>
                 <li class="list-group-item">${manager.getOfficeNumber()}</li>
-                <li class="list-group-item">${manager.getEmail()}</li>
+                <li class="list-group-item"></li>
             </ul>
             <div class="card-body">
-                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">${manager.getEmail()}</a>
                 <a href="#" class="card-link">Another link</a>
             </div>
         </div>`;
     };
-
+// HTML outline for Engineer card information.
     const makeEngineer = (engineer) => {
-
+        return `
+        <div class="card col d-flex justify-content-center p-3 m-5" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${engineer.getName()}</h5>
+                <p class="card-text">${engineer.getRole()}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">${engineer.getId()}</li>
+                <li class="list-group-item"></li>
+                <li class="list-group-item"></li>
+            </ul>
+            <div class="card-body">
+                <a href="#" class="card-link">${engineer.getEmail()}</a>
+                <a href="#" class="card-link">${engineer.getGithub()}</a>
+            </div>
+        </div>`;
     };
+// HTML outline for Intern card information.
+    const makeIntern = (intern) => {
+        return `
+        <div class="card col d-flex justify-content-center p-3 m-5" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${intern.getName()}</h5>
+                <p class="card-text">${intern.getRole()}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">${intern.getId()}</li>
+                <li class="list-group-item">${intern.getSchool()}</li>
+                <li class="list-group-item"></li>
+            </ul>
+            <div class="card-body">
+                <a href="#" class="card-link">${intern.getEmail()}</a>
+                <a href="#" class="card-link"></a>
+            </div>
+        </div>`;
+    };
+
+    // Uses a filter to gather the three roles and add them to the team array.
+    const team = [];
+    team.push (
+        empCard
+            .filter((employee) => employee.getRole() === "Manager")
+            .map((manager) => makeManager(manager))
+            .join("")
+    );
+    team.push (
+        empCard
+            .filter((employee) => employee.getRole() === "Engineer")
+            .map((engineer) => makeEngineer(engineer))
+            .join("")
+    );
+    team.push (
+        empCard
+            .filter((employee) => employee.getRole() === "Intern")
+            .map((intern) => makeIntern(intern))
+            .join("")
+    );
+    return team.join("");
 };
 
+// Containes the html outline for the webpage and exports it.
 module.exports = (empCard) => {
     return `
     <!DOCTYPE html>
